e24316fb825dfff185285417834877024066695f
Josh Bohde <josh.bohde@gmail.com>
2011-04-05 21:39:39 -0500

    Field only pass for EmbeddedListFieldTest.test_post

--- src/eg/api/mongo_fields.py
+++ src/eg/api/mongo_fields.py
@@ -19,27 +19,32 @@ class ListField(ApiField):
         return value 
 
 class EmbeddedListField(ToManyField):
+    is_related = False
+    is_m2m = False
 
- def dehydrate(self, bundle):
-     if not bundle.obj or not bundle.obj.pk:
-         if not self.null:
-            raise ApiFieldError("The model '%r' does not have a primary key and can not be d in a ToMany context." % bundle.obj)
-         return []
-     if not getattr(bundle.obj, self.attribute):
-         if not self.null:
-             raise ApiFieldError("The model '%r' has an empty attribute '%s' and doesn't all a null value." % (bundle.obj, self.attribute))
-         return []
-     self.m2m_resources = []
-     m2m_dehydrated = []
-     # TODO: Also model-specific and leaky. Relies on there being a
-     #       ``Manager`` there.
-     # NOTE: only had to remove .all()
-     for m2m in getattr(bundle.obj, self.attribute):
-         m2m_resource = self.get_related_resource(m2m)
-         m2m_bundle = Bundle(obj=m2m)
-         self.m2m_resources.append(m2m_resource)
-         m2m_dehydrated.append(self.dehydrate_related(m2m_bundle, m2m_resource))
-     return m2m_dehydrated
+    def dehydrate(self, bundle):
+        if not bundle.obj or not bundle.obj.pk:
+            if not self.null:
+                raise ApiFieldError("The model '%r' does not have a primary key and can not be d in a ToMany context." % bundle.obj)
+            return []
+        if not getattr(bundle.obj, self.attribute):
+            if not self.null:
+                raise ApiFieldError("The model '%r' has an empty attribute '%s' and doesn't all a null value." % (bundle.obj, self.attribute))
+            return []
+        self.m2m_resources = []
+        m2m_dehydrated = []
+        # TODO: Also model-specific and leaky. Relies on there being a
+        #       ``Manager`` there.
+        # NOTE: only had to remove .all()
+        for m2m in getattr(bundle.obj, self.attribute):
+            m2m_resource = self.get_related_resource(m2m)
+            m2m_bundle = Bundle(obj=m2m)
+            self.m2m_resources.append(m2m_resource)
+            m2m_dehydrated.append(self.dehydrate_related(m2m_bundle, m2m_resource))
+        return m2m_dehydrated
+
+    def hydrate(self, bundle):
+        return [b.obj for b in self.hydrate_m2m(bundle)]
 
 class DictField(ApiField):
     dehydrated_type     =   'dict'
